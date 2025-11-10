1bc514675b5c9536a931d0de3be3f24473fde7f0
dongdong7-max <dongdong7@handong.ac.kr>
2025-11-10 10:42:22 +0900

    Hello 위치 수정

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Hello />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
