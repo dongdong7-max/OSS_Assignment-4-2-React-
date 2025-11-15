281bb5e9af06229ea6e1dcb5e5cbfc4f1087ea7d
dongdong7-max <dongdong7@handong.ac.kr>
2025-11-15 23:53:31 +0900

    랜더링 수정

--- src/index.js
+++ src/index.js
@@ -17,15 +17,15 @@ const myElement4 = (
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-//  <>
-//  {myElement1}
-//  {myElement2}
-//  {myElement3}
-//  {myElement4}
-//  </>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+ <>
+ {myElement1}
+ {myElement2}
+ {myElement3}
+ {myElement4}
+ </>
  
   
 );
